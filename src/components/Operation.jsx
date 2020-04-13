3edda65937cacba14da68178b7e91ee47736a3d6
Jacob <jacob.simon01@gmail.com>
2020-04-12 17:30:11 -0700

    display schemas

--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Field, Formik, Form } from "formik";
-import { Input, Button, Spin } from "antd";
+import { Input, Button, Spin, Row, Col } from "antd";
 
-import { useOperation } from "../hooks/SwaggerContext";
+import { useOperation, useDefinition } from "../hooks/SwaggerContext";
 
 const Operation = ({ operationJson, path, method }) => {
   const parameters = operationJson.parameters || [];
-  const contentTypeOptions = operationJson.produces;
+  const contentTypeOptions = operationJson.consumes;
   const contentType = (contentTypeOptions || [])[0];
+  const acceptOptions = operationJson.produces;
+  const accept = (acceptOptions || [])[0];
   const [
     call,
     { loading, data, error, responseHeaders, statusCode },
   ] = useOperation();
+  const bodyParam = parameters.find((p) => p.in === "body");
+  const bodyDef = useDefinition(bodyParam && bodyParam.schema["$ref"]);
   return (
     <div>
       <h2>Parameters</h2>
@@ -21,7 +25,7 @@ const Operation = ({ operationJson, path, method }) => {
           call(path, {
             params: values,
             method,
-            headers: { accept: contentType },
+            headers: { "content-type": contentType, accept },
           });
         }}
         initialValues={{}}
@@ -29,12 +33,34 @@ const Operation = ({ operationJson, path, method }) => {
         {(props) => (
           <Form>
             {parameters.map((parameter) => (
-              <div className="form-item" key={parameter.name}>
-                <label>
-                  {parameter.name} - {parameter.description}
-                </label>
-                <Field name={`${parameter.in}.${parameter.name}`} as={Input} />
-              </div>
+              <>
+                <Row className="form-item" key={path + parameter.name}>
+                  <label>
+                    {parameter.name} - {parameter.description}
+                  </label>
+                </Row>
+                <Row>
+                  {parameter.in === "body" ? (
+                    <>
+                      <Col span={12}>
+                        <Field name="body" as={Input.TextArea}></Field>
+                      </Col>
+                      <Col span={12}>
+                        <pre>
+                          {JSON.stringify(bodyDef || parameter.schema, null, 2)}
+                        </pre>
+                      </Col>
+                    </>
+                  ) : (
+                    <Col span={12}>
+                      <Field
+                        name={`${parameter.in}.${parameter.name}`}
+                        as={Input}
+                      />
+                    </Col>
+                  )}
+                </Row>
+              </>
             ))}
             <Button htmlType="submit">Execute</Button>
           </Form>
--- src/hooks/SwaggerContext.jsx
+++ src/hooks/SwaggerContext.jsx
@@ -59,24 +59,50 @@ const useOperation = () => {
   ] = useAjax();
   const [scheme] = useScheme();
   const [baseUrl] = useBaseUrl();
-  const wrappedCall = (url, { params = {}, ...rest }) => {
+  const wrappedCall = (url, { params = {}, headers = {}, ...rest }) => {
     let fullUrl = baseUrl + url;
     Object.entries(params.path || {}).forEach(([k, v]) => {
       fullUrl = fullUrl.replace(`{${k}}`, v);
     });
     let bodyString = null;
     const { body } = params;
-    if (body != null) {
-      // From the spec: 'the name of the body param is for informational purposes only
-      const bodyString = Object.values(body)[0];
+    const { formData } = params;
+    let form;
+    if (formData) {
+      if (headers["content-type"] === "application/x-www-form-urlencoded") {
+        // TODO: use a standardized query string encoder for this
+        form = Object.entries(formData)
+          .map(([k, v]) => `${k}=${v}`)
+          .join("&");
+      } else {
+        form = new FormData();
+        Object.entries(formData).forEach(([k, v]) => {
+          form.append(k, v);
+        });
+      }
     }
     call(fullUrl, {
       ...rest,
       params: params.query,
-      data: JSON.parse(bodyString),
+      data: form || JSON.parse(body),
+      headers,
     });
   };
   return [wrappedCall, { loading, data, error, responseHeaders, statusCode }];
 };
 
-export { SwaggerProvider, useConfigFetcher, useConfig, useOperation };
+const useDefinition = (path = "") => {
+  const [{ definitions }] = useConfig();
+  const defnKey = path.replace("#/definitions/", "");
+  // intentionally returning undefined when no path is passed in so we don't need to check the path
+  // exists at the call site
+  return definitions[defnKey];
+};
+
+export {
+  SwaggerProvider,
+  useConfigFetcher,
+  useConfig,
+  useOperation,
+  useDefinition,
+};
